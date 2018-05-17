a072825702ae9dd34d78dd0279441cdc7964e8c2
Dev1 <Dev1@Fausto-Davila.local>
2018-05-17 13:25:14 +0200

    fff

---
+++ js/imc.js
@@ -0,0 +1,5 @@
+//dEFINIR UN CONSTRUCTOR DE PERSONA CON LOS ATRIBUTOS NECESARIOS PARA PODER USARLO EN EL EJEMPLO DE 31IMC.HTML
+function Persona (peso, altura){
+    this.peso=peso;
+    this.altura=altura;
+}
--- js/persona.js
+++ js/persona.js
@@ -1,22 +1,41 @@
-function Dni (numero1){
-    this.numero = numero1;
+function Dni(numero1) { //funcion constructora
+    this.numero = numero1;//se refiere a la variable el this
 }
-Dni.prototype = 
-{
-    calcularLetra : function (){
-        var letra = "";
-        var SECUENCIA_DNI = "TRWAGMYFPDXBNJZSQVHLCKE";
 
-        letra = SECUENCIA_DNI.charAt(this.numero%23);
-        return letra;
+Dni.prototype =
+    {
+        calcularLetra: function () {
+            var letra = "";
+            var SECUENCIA_DNI = "TRWAGMYFPDXBNJZSQVHLCKE";
+
+            letra = SECUENCIA_DNI.charAt(this.numero % 23);
+            return letra;
+        }
     }
-}
-var dni = new Dni (47310008);
-console.log (dni.numero);
-console.log ("proto dni");
-console.log (dni.__proto__);
-var letra = dni.calcularLetra();
-console.log (letra);
 
+//defino un metodo estatico
+var cadena = dni + letra;
+Dni.esValido = function (cadena) {
+    var valido = false;
+    var longitud = cadena.lenght;
+        if (longitud <9){
+            valido=false;
+            console.log("el dni no es correcto, faltan digitos");
+        }
+        else{
+            valido=true;
+            console.log("El dni es correcto")
+        }
+    return valido;
+}
+
+var dni = new Dni(47310008);
+console.log(dni.numero);
+console.log("proto dni");
+console.log(dni.__proto__);
+var letra = dni.calcularLetra();
+console.log(letra);
+var resultado = Dni.esValido(9);
+console.log (resultado);
 
 
